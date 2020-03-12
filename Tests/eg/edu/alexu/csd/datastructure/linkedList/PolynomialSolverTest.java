c80bb587bedf587233228af8a02a091c98c40213
BerryYoghurt <58439150+BerryYoghurt@users.noreply.github.com>
2020-03-13 00:19:40 +0200

    Reformatted the project
    Changes:
    - The most significant change is that I renamed the packages and sorted the classes into files.. I didn't intend to, but when I downloaded master there were duplicates of files and it was a terrible mess, so I just copied the working code into a new project.
    - Finished multiply as planned and tested it
    -Added a new "add" method to SLinkedList . Any change made in this module has a comment explaining why I made it.
    -Tried to start working on "main", but this interface is so illogical.. I will probably upload a separate document with the inconsistencies I see.

---
+++ Tests/eg/edu/alexu/csd/datastructure/linkedList/PolynomialSolverTest.java
@@ -0,0 +1,83 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.jupiter.api.Test;
+
+class PolynomialSolverTest {
+
+	/*just knowing it works; will add tests later trying to cover all cases*/
+    //@Test
+    /*void setPolynomial() throws FileNotFoundException {
+
+    	try(Scanner sc = new Scanner(new File(System.getProperty("user.dir") +
+                "\\src\\eg\\edu\\alexu\\csd\\datastructure\\linkedList\\Tests\\Resources\\Polynomials.txt")); ){
+    		String sPoly = sc.nextLine();
+            if (sPoly.length() != 1) throw new RuntimeException("Invalid variable name");
+    	}
+        
+    }*/
+
+    @Test
+    void add () {
+        int[][] testArr1 = {{3,4,1,-1}, {6,-2,10,2}};
+        int[][] testArr2 = {{1,2,1}, {2,1,0}};
+        PolynomialSolver ps = new PolynomialSolver();
+        ps.setPolynomial('A', testArr1);
+        ps.setPolynomial('B', testArr2);
+        System.out.println(ps.print('a'));
+        System.out.println(ps.print('b'));
+        ps.add('a', 'b');
+        System.out.println(ps.print('r'));
+        ps.subtract('a', 'b');
+        System.out.println(ps.print('r'));
+        assertFalse(ps.isEmpty('a'));
+        ps.clearPolynomial('a');
+        assertTrue(ps.isEmpty('a'));
+        ps.setPolynomial('a', testArr2);
+        ps.setPolynomial('b', testArr1);
+        assertFalse(ps.isEmpty('a'));
+        System.out.println();
+        System.out.println(ps.print('a'));
+        System.out.println(ps.print('b'));
+    }
+	
+	@Test
+	void multiply() {
+		System.out.println();
+		int[][] testArr1 = {{3,4,1,-1}, {6,-2,10,2}};
+        int[][] testArr2 = {{1,2,1}, {2,1,0}};
+        PolynomialSolver ps = new PolynomialSolver();
+        ps.setPolynomial('A', testArr1);
+        ps.setPolynomial('B', testArr2);
+        System.out.print("A=");
+        System.out.println(ps.print('a'));
+        System.out.print("B=");
+        System.out.println(ps.print('b'));
+        ps.multiply('a', 'b');
+        System.out.print("A*B=");
+        System.out.println(ps.print('r'));
+	}
+	
+	/*@Test
+	void test() {
+		DLinkedList<PolynomialSolver.Term> p1 = new DLinkedList<PolynomialSolver.Term>(PolynomialSolver.Term.class);
+		DLinkedList<PolynomialSolver.Term> p2;
+		DLinkedList<PolynomialSolver.Term> p3 = new DLinkedList<PolynomialSolver.Term>(PolynomialSolver.Term.class);
+
+		p1.add(new PolynomialSolver.Term(1,1));
+		p1.add(new PolynomialSolver.Term(1,0));
+		p2 = p1.sublist(0, 1);
+		
+		p3.add(new PolynomialSolver.Term(1,2));
+		p3.add(new PolynomialSolver.Term(2,1));
+		p3.add(new PolynomialSolver.Term (1,0));
+		assertIterableEquals(p3,p.multiply(p1,p2)); 
+	}*/
+
+}
---
+++ Tests/eg/edu/alexu/csd/datastructure/linkedList/SLinkedListTest.java
@@ -0,0 +1,80 @@
+package eg.edu.alexu.csd.datastructure.linkedList;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SLinkedListTest {
+
+    @Test
+    void add() {
+        SLinkedList<String> ll = new SLinkedList<>();
+        ll.add("first");
+        ll.add("second");
+        ll.add(1, "new Entry");
+        ll.add(2, "another One");
+        assertEquals(ll.get(0), "first");
+        assertEquals(ll.get(1), "new Entry");
+        assertEquals(ll.get(2), "another One");
+        assertEquals(ll.get(3), "second");
+        ll.set(2, "test set");
+        assertEquals(ll.get(2), "test set");
+        assertEquals(ll.size(), 4);
+        assertThrows(ArrayIndexOutOfBoundsException.class, ()->ll.get(4));
+        ll.remove(1);
+        assertEquals(ll.get(2), "second");
+        assertEquals(ll.size(), 3);
+        assertFalse(ll.isEmpty());
+        assertTrue(ll.contains("first"));
+        assertFalse(ll.contains("another One"));
+        assertTrue(ll.contains("test set"));
+        assertFalse(ll.contains(5));
+        ll.clear();
+        assertTrue(ll.isEmpty());
+        ll.add("first");
+        ll.add("second");
+        ll.add(1, "new Entry");
+        ll.add(2, "another One");
+        assertEquals(ll.get(0), "first");
+        assertEquals(ll.get(1), "new Entry");
+        assertEquals(ll.get(2), "another One");
+        assertEquals(ll.get(3), "second");
+    }
+    @Test
+    @SuppressWarnings("unchecked")
+    void subList() {
+        SLinkedList<Integer> ll = new SLinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            ll.add(i);
+        }
+        SLinkedList<Integer> testLL = (SLinkedList<Integer>) ll.sublist(3, 7);
+        assertEquals(testLL.size(), 5);
+        assertEquals(testLL.get(0), 3);
+        assertThrows(ArrayIndexOutOfBoundsException.class, ()-> ll.sublist(0, 20));
+    }
+
+    @Test
+    void getNext() {
+        SLinkedList<Integer> ll = new SLinkedList<>();
+        for (int i = 0; i < 5; i++) {
+            ll.add(i);
+        }
+        assertTrue(ll.hasNext());
+        assertEquals(ll.getNext(), 0);
+        assertTrue(ll.hasNext());
+        assertEquals(ll.getNext(), 1);
+        assertEquals(ll.get(3), 3);
+        assertTrue(ll.hasNext());
+        assertEquals(ll.getNext(), 4);
+        assertFalse(ll.hasNext());
+        assertThrows(ArrayIndexOutOfBoundsException.class, ll::getNext);
+    }
+
+    @Test
+    void objectList() {
+        SLinkedList<Object> ll = new SLinkedList<>();
+        ll.add("StringSS");
+        ll.add(6);
+        assertEquals(((String)(ll.get(0))).substring(6), "SS");
+        assertEquals(((Integer) ll.get(1)).toString(), "6");
+    }
+
+}
